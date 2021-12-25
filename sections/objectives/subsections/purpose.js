1a917121a869ce53c7ff7f64f65ee202b8204ddf
SergeyBibikov <sergey.bibikov.92@mail.ru>
2021-12-25 21:38:21 +0300

    Стиль

--- sections/objectives/subsections/purpose.js
+++ sections/objectives/subsections/purpose.js
@@ -8,6 +8,69 @@ module.exports = {
                 "Executing tests",
                 "Comparing actual outcomes to predicted outcomes"
             ]
-        }
+        },
+        {
+            name: "Testware consists of:",
+            pointSummary: [
+                "Software",
+                "Documentation",
+                "Test cases",
+                "Test environment",
+                "Test data",
+            ]
+        },
+        {
+            name: "Interaction with a SUT via:",
+            pointSummary: [
+                "API (low-level)",
+                "GUI (high-level)",
+                "Service or protocol"
+            ]
+        },
+        {
+            name:"Test automation objectives:",
+            pointSummary: [
+                "Improving test efficiency",
+                "Providing wider function coverage",
+                "Reducing total test cost",
+                "Performing tests that manual testers cannot",
+                "Shortening the test execution period",
+                "Increasing the test frequency/reducing the time required for test cycles"
+            ]
+        },
+        {
+            name:"Advantages of test automation:",
+            pointSummary:[
+                "More tests can be run per build",
+                `The possibility to create tests 
+                    that cannot be done manually (real-time, remote, parallel tests)`,
+                "Tests can be more complex",
+                "Tests run faster",
+                "Tests are less subject to operator error",
+                "More effective and efficient use of testing resources",
+                "Quicker feedback regarding software quality",
+                "Improved system reliability (e.g., repeatability, consistency)",
+                "Improved consistency of tests",
+            ]
+        },
+        {
+            name:"Disadvantages of test automation:",
+            pointSummary: [
+                "Additional costs are involved",
+                "Initial investment to setup TAS",
+                "Requires additional technologies",
+                "Team needs to have development and automation skills",
+                "On-going TAS maintenance requirement",
+                `Can distract from testing objectives, 
+                    e.g., focusing on automating tests cases at the expense of
+                    executing tests`,
+                "Tests can become more complex",
+                "Additional errors may be introduced by automation"
+            ],
+        },
+        {
+            name:"",
+            pointSummary: []
+        },
     ],
 }
