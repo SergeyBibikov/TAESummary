576023588426bfab663ce4ba1d7b5ca90e30ce9a
SergeyBibikov <sergey.bibikov.92@mail.ru>
2021-12-26 12:46:05 +0300

    prep. all

--- sections/preparation/subsections/designfortestability.js
+++ sections/preparation/subsections/designfortestability.js
@@ -2,9 +2,28 @@ module.exports = {
     name: "Design for Testability and Automation.",
     keyPoints:[
         {
-            name:"",
+            name:"Design for testability parts:",
             pointSummary:[
-                
+               "Observability (insight into the system)", 
+               "Control(ability)", 
+               "Clearly defined architecture", 
+            ]
+        },
+        {
+            name:"Software interfaces that support testing",
+            pointSummary:[
+               "Spreadsheets scripting", 
+               "Stubs and mocks",
+               "Software interfaces (instead of real devices): help to simulate hardware failures", 
+               "Interfaces for SUT state querying", 
+            ]
+        },
+        {
+            name:"Design for automation should consider that:",
+            pointSummary:[
+               "Compatibility with existing test tools should be established early on.",
+               "Test tool compatibility with the SUT details is critical",
+               "Solutions may require development of program code and calls to APIs"
             ]
         }
     ]
--- sections/preparation/subsections/toolevaluation.js
+++ sections/preparation/subsections/toolevaluation.js
@@ -2,9 +2,28 @@ module.exports = {
     name: "Tool Evaluation and Selection",
     keyPoints:[
         {
-            name:"",
+            name:"TAE contribution to choosing the right tool",
             pointSummary:[
-                
+                "Assessing organizational maturity and identification of opportunities for test tool support",
+                "Assessing appropriate objectives for test tool support",
+                "Identifying and collecting information on potentially suitable tools",
+                "Analyzing tool information against objectives and project constraints",
+                "Estimating the cost-benefit ratio based on a solid business case",
+                "Making a recommendation on the appropriate tool",
+                "Identifying compatibility of the tool with SUT components",
+            ]
+        },
+        {
+            name: "General tool advice:",
+            pointSummary: [
+                "Use well-known tools",
+                "Get familiar with the tool before using it in the TAS",
+                "Make sure tool has all the capabilities necessary",
+                "Make sure tool does not have too much functions that you will not need",
+                "Read tool's release notes",
+                "Visit forums where the tool is discussed",
+                "Upgrade the tool as needed",
+                "Minimize the dependency on the tool"
             ]
         }
     ]
