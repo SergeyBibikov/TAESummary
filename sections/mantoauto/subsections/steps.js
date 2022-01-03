8559c5ef668cba4cae3fa5afdc4f6e2a1044430c
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 18:18:17 +0300

    6.2

--- sections/mantoauto/subsections/steps.js
+++ sections/mantoauto/subsections/steps.js
@@ -1,10 +1,20 @@
 module.exports = {
-    name: "",
+    name: "Identify Steps Needed to Implement Automation within Regression Testing",
     keyPoints: [
         {
-            name: "",
+            name: "Questions to answer before implementing regression tests automation:",
             pointSummary:[
-                ``,
+                `How frequently should the tests be run?`,
+                `What is the execution time for each test, for the regression suite?`,
+                `Is there functional overlap between tests?
+                Overlapping tests should be combined into smaller number of tests`,
+                `Do tests share data?
+                Shared date should be placed separately to avoid duplication`,
+                `Are the tests dependent on each other?`,
+                `What pre-conditions are required before test execution?`,
+                `What % of SUT coverage do the tests represent?`,
+                `Do the tests currently execute without failure?`,
+                `What should happen when regression tests take too long?`
             ]
         }
     ]
