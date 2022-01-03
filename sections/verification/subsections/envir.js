9195b16fd5bc08c8b7265ed42d661fa052057b70
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 19:27:16 +0300

    7.1

--- sections/verification/subsections/envir.js
+++ sections/verification/subsections/envir.js
@@ -2,9 +2,23 @@ module.exports = {
     name: "Verifying Automated Test Environment Components",
     keyPoints: [
         {
-            name: "",
+            name: "Steps to verify the components of the automated test environment:",
             pointSummary:[
-                ``,
+                `Test tool installation, setup, configuration, and customization`,
+                `Test scripts with known passes and failures`,
+                `Repeatability in setup/teardown of the test environment`,
+                `Configuration of the test environment and components`,
+                `Connectivity against internal and external systems/interfaces`,
+                `Intrusiveness of automated test tools`,
+                `Framework component testing (as the TAF is a piece of software)`,
+            ]
+        },
+        {
+            name: "Levels of intrusion (low to high)",
+            pointSummary: [
+                `Interacting with the SUT from external interfaces (best end user simulation)`,
+                `GUI level interaction`,
+                `Test interfaces interaction`,
             ]
         }
     ]
--- sections/verification/subsections/suite.js
+++ sections/verification/subsections/suite.js
@@ -2,9 +2,13 @@ module.exports = {
     name: "Verifying the Automated Test Suite",
     keyPoints: [
         {
-            name: "",
+            name: "Steps to verify the automated test suite:",
             pointSummary:[
-                ``,
+                `Executing test scripts with known passes and failures`,
+                `Checking the test suite`,
+                `Verifying new tests that focus on new features of the framework`,
+                `Considering the repeatability of tests`,
+                `Checking that there are enough verification points in the automated test suite.`
             ]
         }
     ]
