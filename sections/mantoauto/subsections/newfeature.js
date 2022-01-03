21c2528c4b4b520dca4194a3df46229b83330540
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 18:31:32 +0300

    6.3

--- sections/mantoauto/subsections/newfeature.js
+++ sections/mantoauto/subsections/newfeature.js
@@ -1,10 +1,18 @@
 module.exports = {
-    name: "",
+    name: "Factors to Consider when Implementing Automation within New Feature Testing",
     keyPoints: [
         {
-            name: "",
+            name: "General advice",
             pointSummary:[
-                ``,
+                `When the feature is not implemented yet, 
+                    it is possible to increase its testability`,
+                `The cases developed by test designers should be analyzed
+                    with regard to the TAS ability to implement them`,
+                `The changes in the TAS, supporting new features, 
+                should not break existing TAS functionality`,
+                `Traceability from the requirements to the test
+                cases will indicate which test cases need to be updated 
+                to support new feature testing`
             ]
         }
     ]
