ce53775b88141860b518189555afd18ec256702e
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 14:38:05 +0300

    5.2

--- sections/reporting/subsections/measurement.js
+++ sections/reporting/subsections/measurement.js
@@ -1,9 +1,25 @@
 module.exports = {
-    name: "",
+    name: "Implementation of Measurement",
     keyPoints: [
         {
-            name:"",
-            pointSummary: ""
+            name:"Features of automation that support measurement and report generation",
+            pointSummary: [
+                `When to log? Before, during and after test execution`,
+                `What test level to log? Individual tests, sets of tests and an entire test suite`,
+                `It's best to highlight the trends`,
+            ]
+        },
+        {
+            name: "Integration with other third party tools",
+            pointSummary: [
+                `Examples of the tools: spreadsheets, XML, documents, databases, report tools`,
+            ]
+        },
+        {
+            name: "Visualization of results",
+            pointSummary: [
+                `Examples of the visualization tools: dashboards, charts, graphs`,
+            ]
         }
     ]
 }
