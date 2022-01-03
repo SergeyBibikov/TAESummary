ae92c826bbdc7342936606e9ffe75f3d9d8996d3
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 20:17:45 +0300

    8

--- sections/improvement/subsections/options.js
+++ sections/improvement/subsections/options.js
@@ -1,12 +1,33 @@
 module.exports = {
-    name: "",
+    name: "Options for Improving Test Automation",
     keyPoints: [
         {
-            name: "",
+            name: "TAS improvement benefits include:",
             pointSummary:[
-                ``,
-                ``,
-                ``
+                `greater efficiency (further reducing manual intervention)`,
+                `better ease of use`,
+                `additional capabilities`,
+                `improved support for testing activities`,
+            ]
+        },
+        {
+            name: "Areas of a TAS that may be considered for improvement:",
+            pointSummary: [
+                `Scripting: <br>
+                * decrease code duplicaition<br>
+                * enrich libs in use<br>
+                * parametrize tests where needed<br>
+                * establish an error recovery process<br>
+                * use dynamic waits or events<br>
+                * treat the testware as software<br>
+                * revise existing scripts and remove outdated ones<br>`,
+                `Test execution(parallel and high-priority tests first)`,
+                `Verification (standard verification methods)`,
+                `Architecture (some SUT changes may trigger the TAA changes)`,
+                `Pre- and post-processing`,
+                `Documentation`,
+                `TAS features(new features - only when needed)`,
+                `TAS updates and upgrades(applied incrementally and tested thoroughly)`,
             ]
         }
     ]
--- sections/improvement/subsections/planning.js
+++ sections/improvement/subsections/planning.js
@@ -1,12 +1,21 @@
 module.exports = {
-    name: "",
+    name: "Planning the Implementation of Test Automation Improvement",
     keyPoints: [
         {
-            name: "",
+            name: "What to bear in mind:",
             pointSummary:[
-                ``,
-                ``,
-                ``
+                `Identify changes in the test environment components:<br>
+                * changes should be made incrementally in small chunks
+                and tested<br>
+                * full regression suite should be launched only after 
+                all the changes are applied<br>`,
+                `Increase efficiency and effectiveness of core TAS function libraries`,
+                `Target multiple functions that act on the same control type for consolidation`,
+                `Refactor the TAA to accommodate changes in the SUT`,
+                `Naming conventions and standardization should be followed 
+                by new libraries`,
+                `Evaluation of existing scripts for SUT revision/elimination
+                (big scripts decomposition, rarely run scripts elimination)`,
             ]
         }
     ]
