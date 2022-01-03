1e9f5821edf9ef66628eedb56acb2935af22c149
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 15:50:09 +0300

    reporting

--- sections/reporting/subsections/reporting.js
+++ sections/reporting/subsections/reporting.js
@@ -1,9 +1,24 @@
 module.exports = {
-    name: "",
+    name: "Test Automation Reporting",
     keyPoints: [
         {
-            name:"",
-            pointSummary: ""
+            name:"Content of the reports",
+            pointSummary: [
+                `The execution results`,
+                `The system being tested`,
+                `The environment in which the tests were run`,
+            ]
+        },
+        {
+            name: "Publishing the reports",
+            pointSummary: [
+                `The report should be published for everyone interested in the execution results`,
+                `Reports can be:<br>
+                * uploaded on a website<br>
+                * sent to a mailing list<br>
+                * uploaded to another tool<br>
+                `
+            ]
         }
     ]
 }
