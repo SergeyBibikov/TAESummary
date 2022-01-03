73b5c236c9a8457fed1d6b15ee3e4bf6280ea314
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-01-03 18:43:46 +0300

    6

--- sections/mantoauto/subsections/confirmation.js
+++ sections/mantoauto/subsections/confirmation.js
@@ -1,10 +1,13 @@
 module.exports = {
-    name: "",
+    name: "Factors to Consider when Implementing Automation of Confirmation Testing",
     keyPoints: [
         {
-            name: "",
+            name: "General advice",
             pointSummary:[
-                ``,
+                `The confirmation test can be added to,
+                and complement, the existing automated regression test bed`,
+                `The automated confirmation test has a narrow scope of functionality`,
+                `It can be implemented before the defect is fixed`
             ]
         }
     ]
